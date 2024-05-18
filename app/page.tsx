a0c012ed9bac66d2893efaa2be7f741c87675beb
0xsonu <45717030+0xsonu@users.noreply.github.com>
2024-05-18 14:00:37 +0530

    Initialize Project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">main Page</div>
+    </main>
+  );
+}
