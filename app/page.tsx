a4c2649b31d31376c3c741f98b2475880eedc468
0xsonu <45717030+0xsonu@users.noreply.github.com>
2024-05-20 21:38:22 +0530

    Adding Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Grid from "@/components/Grid";
+
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">main Page</div>
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+      </div>
     </main>
   );
-}
+};
+
+export default Home;
